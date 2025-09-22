402529c9489a35a8ffd862930d9a5c55fdf9df5c
rahintoshmi <2204ohee@gmail.com>
2025-09-22 10:25:32 +0600

    p2

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,23 @@ function App() {
     <>
       <h1>Vite + React</h1>
       <p>I am Ohee</p>
+      <Reuse></Reuse>
+      <Player></Player>
       
     </>
+  )
+}
+function Reuse(){
+  return (
+    <h3>I am rahin toshmi ohee</h3>
+  )
+}
+function Player(){
+  const age=21;
+  const name = 'ohee';
+  return(
+    <p>I am : {name} , my age : {age}</p>
+
   )
 }
 
